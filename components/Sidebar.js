b2c523aab680d50fa281c43814917a93ba76c262
Ric Taglietti <rictaglietti@gmail.com>
2021-12-09 18:43:22 -0500

    addd center header

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -12,7 +12,7 @@ function Sidebar() {
   const { data: session, status } = useSession();
   console.log(session);
   return (
-    <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
+    <div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen">
       <div className="space-y-4">
         <button
           className="flex items-center space-x-2 hover:text-white"
@@ -47,6 +47,11 @@ function Sidebar() {
         </button>
         <hr className="border-t-[0.1px] border-gray-900" />
         {/* Playlist*/}
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
         <p className="cursor-pointer hover:text-white">Playlist name...</p>
         <p className="cursor-pointer hover:text-white">Playlist name...</p>
         <p className="cursor-pointer hover:text-white">Playlist name...</p>
