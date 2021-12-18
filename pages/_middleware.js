e317d0bd136bcef6113f139e34c8d83a39322279
Ric Taglietti <rictaglietti@gmail.com>
2021-12-17 19:19:48 -0500

    deploy bug

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -6,9 +6,9 @@ export async function middleware(req) {
   const token = await getToken({
     req,
     secret: process.env.JWT_SECRET,
-    // secureCookie:
-    //   process.env.NEXTAUTH_URL?.startsWith('https://') ??
-    //   !!process.env.VERCEL_URL,
+    secureCookie:
+      process.env.NEXTAUTH_URL?.startsWith('https://') ??
+      !!process.env.VERCEL_URL,
   });
 
   const { pathname } = req.nextUrl;
