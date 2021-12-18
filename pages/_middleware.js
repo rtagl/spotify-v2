e29b2e141956e190c3a52feffbcdd009dba5f8ff
Ric Taglietti <rictaglietti@gmail.com>
2021-12-17 19:31:50 -0500

    final

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -6,9 +6,6 @@ export async function middleware(req) {
   const token = await getToken({
     req,
     secret: process.env.JWT_SECRET,
-    secureCookie:
-      process.env.NEXTAUTH_URL?.startsWith('https://') ??
-      !!process.env.VERCEL_URL,
   });
 
   const { pathname } = req.nextUrl;
